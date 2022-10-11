6c6b385ab9ec7328f7a1a570943e5a5ba1740c22
Erik Lucht <44031383+elucht@users.noreply.github.com>
2022-10-11 13:27:31 -0500

    Delete Nonprofit Data Warehouse Quickstart/NonprofitDataWarehouseQuickstartWithCDM directory

--- Nonprofit Data Warehouse Quickstart/NonprofitDataWarehouseQuickstartWithCDM/Warehouse/Warehouse/External Resources/External File Formats/ExternalFileFormatParquet.sql
+++
@@ -1,9 +0,0 @@
--- Copyright (c) Microsoft Corporation.
--- Licensed under the MIT License.
-
--- CREATE AN EXTERNAL FILE FORMAT
-CREATE EXTERNAL FILE FORMAT [ExternalFileFormatParquet] WITH 
-(  
-	FORMAT_TYPE = PARQUET,
-	DATA_COMPRESSION = 'org.apache.hadoop.io.compress.SnappyCodec'
-)
--- Nonprofit Data Warehouse Quickstart/NonprofitDataWarehouseQuickstartWithCDM/Warehouse/Warehouse/Security/Database Scoped Credential/DatabaseScopedCredentialADLS.sql
+++
@@ -1,8 +0,0 @@
-﻿-- Copyright (c) Microsoft Corporation.
--- Licensed under the MIT License.
-
--- CREATE A DATABASE SCOPED CREDENTIAL
-
-CREATE DATABASE SCOPED CREDENTIAL [DatabaseScopedCredentialADLS] WITH
-    IDENTITY = 'Managed Service Identity'
-;
